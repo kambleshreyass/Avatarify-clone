9c36be4fb2f607ac8af12e8c70bc64a89de9ed56
Shreyas Kamble <kshreyass59@gmail.com>
2024-08-06 23:15:53 +0530

    install sass

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navigation from "../src/components/Navigation"
+
+const App = () => {
+  return (
+    <>
+    <Navigation/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+
+
+const Navigation = () => {
+  return (
+    <div id="nav">
+      <div>
+        <img src={"https://static.tildacdn.com/tild3064-6539-4535-a333-333165343133/Group_14.svg"} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Navigation
