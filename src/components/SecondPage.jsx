dfec0014b0d8dc6ae71770830a5936776b46d6dd
Shreyas Kamble <kshreyass59@gmail.com>
2024-08-07 16:56:20 +0530

    app work in progress

--- src/components/SecondPage.jsx
+++ src/components/SecondPage.jsx
@@ -2,7 +2,20 @@
 
 const SecondPage = () => {
   return (
-    <div id="second"></div>
+    <div id="second">
+      <h1>Look at our <span>users</span> family <span><img src={"https://optim.tildacdn.com/tild3361-6339-4364-a132-306332336363/-/resize/85x/-/format/webp/Heart.png"} alt="" /></span> </h1>
+      <p>Many of our users share videos with hashtag <span>#Avatarify</span></p>
+
+      <div className="img-container">
+        <img src={"https://optim.tildacdn.com/tild3137-3130-4333-b632-653162353636/-/resize/744x/-/format/webp/Frame_21.png"} alt="" />
+
+        <img src={"https://optim.tildacdn.com/tild6534-6634-4933-a661-343663653337/-/resize/600x/-/format/webp/Frame_22.png"} alt="" />
+        <img src={"https://optim.tildacdn.com/tild3535-6634-4333-b736-633363336232/-/resize/600x/-/format/webp/Frame_14.png"} alt="" />
+      
+      </div>
+    </div>
+
+    
   )
 }
 
